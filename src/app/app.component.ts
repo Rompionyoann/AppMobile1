860d38e6acc2efb330e5cac6d9ddefa0207a97c8
Rompionyoann <45792453+Rompionyoann@users.noreply.github.com>
2023-01-18 10:33:03 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, bindCallback, map ,  } from 'rxjs';
+import {Geolocation, Position } from '@capacitor/geolocation';
+import { Plugins } from '@capacitor/core'; 
+const modals = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AppMobile';
+  coords =  GeolocationPosition;
+
+  ngOnInit(): void {
+    this.watchPosition().subscribe( coords =>
+      this.coords =coords
+      );
+    
+  }
+   watchPosition(): Observable<any>{
+     const watch = bindCallback(Geolocation.watchPosition)({});
+     return watch.pipe(map(pos => pos[0]));
+
+   }
+
+   showAlert(){
+      
+   }
+  
 }
